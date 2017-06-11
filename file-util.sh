07c8c3d4637e66518b1771f9dc644734f94560e4
Edward Ost <eost@talend.com>
2017-06-11 17:41:39 -0400

    updated file-util to set permissions g+rx

--- file-util.sh
+++ file-util.sh
@@ -63,8 +63,10 @@ function create_user_directory() {
     local owner="${2:-${installUser:-$USER}}"
     local group="${3:-${installUser:-$(id -gn)}}"
 
+    # todo: use iteration rather than recursion
     [ ! -d "${parentDir}" ] && create_user_directory "${parentDir}" "${owner}" "${group}"
     sudo mkdir -p "${fullDirPath}"
+    chmod g+rx "${fullDirPath}"
     sudo chown "${owner}:${group}" "${fullDirPath}"
 
 }
